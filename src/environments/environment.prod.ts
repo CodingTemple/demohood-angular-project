35bd818de41242783928303c3cdb1887e80534c7
carter3689 <carter3689@gmail.com>
2020-01-29 09:41:26 -0600

    Creating first version of project for student reference if needed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCyltO8yb_I9NiFr9-KGBNBqPy_s-LiUbk",
+    authDomain: "demohood-jc.firebaseapp.com",
+    databaseURL: "https://demohood-jc.firebaseio.com",
+    projectId: "demohood-jc",
+    storageBucket: "demohood-jc.appspot.com",
+    messagingSenderId: "813478992417",
+    appId: "1:813478992417:web:fd3dc5a81d0993a3b46160"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCyltO8yb_I9NiFr9-KGBNBqPy_s-LiUbk",
+    authDomain: "demohood-jc.firebaseapp.com",
+    databaseURL: "https://demohood-jc.firebaseio.com",
+    projectId: "demohood-jc",
+    storageBucket: "demohood-jc.appspot.com",
+    messagingSenderId: "813478992417",
+    appId: "1:813478992417:web:fd3dc5a81d0993a3b46160"
+  }
 };
 
 /*
